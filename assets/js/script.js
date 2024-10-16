792664887027f17162e529b04d9d516f12449591
qwjay78 <joana.lps.pro@gmail.com>
2024-10-16 15:45:23 +0200

    Corrections des erreurs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,44 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    let is_run    = "true";
+    let is_run = true; 
 
     init();
 
-    function init(){
-       document.querySelector('.button').addEventListener('clic', (event) => 
-            is_run = !is_run
-            ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? dcument.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class')+' pause');
-        })
+    function init() {
+        // Correction de 'click'
+        document.querySelector('.button').addEventListener('click', (event) => {
+            is_run = !is_run;
 
-        setTimeInterval(function(){
-            if(is_run){
-                let oDate = new Date();
-                document.querySelector('#hours').innerHTML   =  adjustTimer(oDate.getHours());
-                document.querySelector('#minutes').innerHTML =  adjustTimer(oDate.getMinutes());
-                document.querySelector('#seconds').innerHTML =  adjustTimer( oDate.getSeconds());
-
-                document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
-                
+            // Correction de 'document'
+            if ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) {
+                document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', ''));
+            } else {
+                document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class') + ' pause');
             }
-        }, 1000;
+        });
+
+        // Correction 
+        setInterval(function () {
+            if (is_run) {
+                let oDate = new Date();
+                document.querySelector('#hours').innerHTML = adjustTimer(oDate.getHours());
+                document.querySelector('#minutes').innerHTML = adjustTimer(oDate.getMinutes());
+                document.querySelector('#seconds').innerHTML = adjustTimer(oDate.getSeconds());
+
+                document.querySelector('body').style.background = randomHexColor(
+                    oDate.getHours(),
+                    oDate.getMinutes(),
+                    oDate.getSeconds()
+                );
+            }
+        }, 1000); 
     }
 
-    function adjustTimer(timer){
-        (timer < 10 ? '0'+timer : timer);
+    function adjustTimer(timer) {
+        return (timer < 10 ? '0' + timer : timer);
     }
 
-    function randomHexColor(x, y){
-        return "rgb(" + Math.floor(x/100 * 256) + "," + Math.floor(y/100 * 256) + "," + Math.floor(z/100 * 256) + ")";
+    function randomHexColor(x, y, z) { 
+        return "rgb(" + Math.floor(x / 100 * 256) + "," + Math.floor(y / 100 * 256) + "," + Math.floor(z / 100 * 256) + ")";
     }
 });
+
